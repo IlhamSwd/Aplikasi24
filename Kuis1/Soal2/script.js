7a5ad7846171607c10debc194165554e392e30f2
IlhamSwd <160827594+IlhamSwd@users.noreply.github.com>
2024-04-25 11:49:31 +0700

    update 1

--- Kuis1/Soal2/script.js
+++ Kuis1/Soal2/script.js
@@ -2,9 +2,11 @@ let npm = 2226240001;
 let nama = "Arif";
 let nilai = [80, 70, 90, 85, 60, 88];
 
+// MEMANGGIL OBJECT
 document.getElementById("npm").innerHTML = npm;
 document.getElementById("nama").innerHTML = nama;
 
+// MENGGUNAKAN ARRAY UNTUK MENACARI NILAI
 let nilaimax = 0;
 for (const [i, d] of nilai.entries()) {
     if (d > nilaimax) {
