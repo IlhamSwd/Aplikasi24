8e59a4e938510fc7b63acd3007e7b4026f2364d9
IlhamSwd <160827594+IlhamSwd@users.noreply.github.com>
2024-05-09 08:56:10 +0700

    up date

--- Kuis1/Soal1/script.js
+++ Kuis1/Soal1/script.js
@@ -2,10 +2,12 @@ let nama = "arif";
 let hobi = ["Baca", "Bermain game", "Belajar Coding"];
 
 document.getElementById("nama").innerHTML = nama;
-document.getElementById("hobi").innerHTML = hobi[2];
+document.getElementById("hobi").innerHTML = hobi[2]; 
+//  DALAM KURUNG 2 KARENA MEMANGGIL SALAH SATU
 
  let data =[];
 
+//  MEMANGGIL DATA 
 for (const [i, data] of hobi.entries()) {
     document.getElementById("listhobi").innerHTML +=`<li>${data}</li>`;
 }
